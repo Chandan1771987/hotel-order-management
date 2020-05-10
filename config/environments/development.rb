80745002e665cbbf14b4948d58d3f0782d885130
Chandan Ashok Marathe <chandan.bmsce@gmail.com>
2020-05-10 21:18:43 +0530

    Third Commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,6 +47,29 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+
+  #config.action_mailer.delivery_method = :sendmail
+  # Defaults to:
+  # config.action_mailer.sendmail_settings = {
+  #   location: '/usr/sbin/sendmail',
+  #   arguments: '-i'
+  # }
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_options = {from: 'your email'}
+
+
+# FOR GMAIL :
+#  config.action_mailer.delivery_method = :smtp
+#  config.action_mailer.smtp_settings = {
+#      address:              'smtp.gmail.com',
+#      port:                 587,
+#      domain:               'example.com',
+#      user_name:            '<your email>',
+#      password:             '<your password>',
+#      authentication:       'plain',
+#      enable_starttls_auto: true }
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
