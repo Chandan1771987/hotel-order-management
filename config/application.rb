f89cd312135a38bdff48e36898ca4cffab0812b5
Chandan Ashok Marathe <chandan.bmsce@gmail.com>
2020-05-16 20:12:13 +0530

    changes

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module HotelOrderManagement
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    #
+     config.secret_key_base = ENV['SECRET_KEY_BASE']
   end
 end
